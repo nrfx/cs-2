347fb0ddff3a859127b1ba6cf922bda09f306f1f
nrfx <zavovaz001@gmail.com>
2026-02-25 14:03:35 +0800

    поменял под массивы чтобы разделить на логику и взаимодействие с пользователем

--- Program.cs
+++ Program.cs
@@ -1,22 +1,31 @@
 ﻿//Задано n троек целых чисел (ai,bi,ci).Определить, сколько троек может быть использовано для построения треугольника со сторонами a_i, b_i, c_i.
 
+//взаимодействие с юзеоом
 Console.WriteLine("введи переменную n (количество троек чисел) ");
 int n = Convert.ToInt32(Console.ReadLine());
 
-int count = 0;
+int[] a = new int[n];
+int[] b = new int[n];
+int[] c = new int[n];
 
 for (int i = 0; i < n; i++)
 {
     Console.WriteLine("введите три числа через enter: ");
-    int a = Convert.ToInt32(Console.ReadLine());
-    int b = Convert.ToInt32(Console.ReadLine());
-    int c = Convert.ToInt32(Console.ReadLine());
-
-    if ((a+b>c) && (a+c>b) && (b + c > a)){
+    a[i] = Convert.ToInt32(Console.ReadLine());
+    b[i] = Convert.ToInt32(Console.ReadLine());
+    c[i] = Convert.ToInt32(Console.ReadLine());
+}
+// логика тут
+int count = 0;
+for (int i = 0; i < n; i++)
+{
+    if (a[i] + b[i] > c[i] && a[i] + c[i] > b[i] && b[i] + c[i] > a[i])
+    {
         count++;
     }
 }
 
+//вывод
 string outMessage = $"Количество троек из которых можно сделать треугольник: {count}";
 Console.WriteLine(outMessage);
 
