2fbc9a8c72885c5fc95fe67ef820204cc86ec8af
nrfx <zavovaz001@gmail.com>
2026-02-25 13:59:15 +0800

    переделал ввод чисел

--- Program.cs
+++ Program.cs
@@ -1,25 +1,22 @@
 ﻿//Задано n троек целых чисел (ai,bi,ci).Определить, сколько троек может быть использовано для построения треугольника со сторонами a_i, b_i, c_i.
 
 Console.WriteLine("введи переменную n (количество троек чисел) ");
-string n_str = Console.ReadLine();
-int n = Convert.ToInt32(n_str);
+int n = Convert.ToInt32(Console.ReadLine());
 
 int count = 0;
 
 for (int i = 0; i < n; i++)
 {
     Console.WriteLine("введите три числа через enter: ");
-    string a_str = Console.ReadLine();
-    int a = Convert.ToInt32(a_str);
-    string b_str = Console.ReadLine();
-    int b = Convert.ToInt32(b_str);
-    string c_str = Console.ReadLine();
-    int c = Convert.ToInt32(c_str);
+    int a = Convert.ToInt32(Console.ReadLine());
+    int b = Convert.ToInt32(Console.ReadLine());
+    int c = Convert.ToInt32(Console.ReadLine());
 
     if ((a+b>c) && (a+c>b) && (b + c > a)){
         count++;
     }
 }
 
-Console.WriteLine($"Количество троек которые являются пифагоровыми: {count}");
+string outMessage = $"Количество троек из которых можно сделать треугольник: {count}";
+Console.WriteLine(outMessage);
 
