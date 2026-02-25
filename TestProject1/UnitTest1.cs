8dc4352981c2598d71e8e4299d68a4b4a33df383
nrfx <zavovaz001@gmail.com>
2026-02-25 14:37:48 +0800

    сделал тесты 5 шт

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,78 @@
+﻿using Triangle;
+
+namespace TestProject1
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+             int[] a = { 3, 1, 2 };
+             int[] b = { 4, 2, 3 };
+             int[] c = { 5, 3, 4 };
+             int n = 3;
+    
+             TriangleCalculator calculator = new TriangleCalculator();
+             int count = calculator.Count(a, b, c, n);
+    
+             Assert.That(count, Is.EqualTo(2));
+        }
+        [Test]
+        public void Test2()
+        {
+            int[] a = { 0, 0 };
+            int[] b = { 0, 0 };
+            int[] c = { 0, 0 }; 
+            int n = 2;
+
+            TriangleCalculator calculator = new TriangleCalculator();
+            int count = calculator.Count(a, b, c, n);
+
+            Assert.That(count, Is.EqualTo(0));
+        }
+        [Test]
+        public void Test3()
+        {
+            int[] a = { 1, 10, 2 };
+            int[] b = { 2, 2, 2 };
+            int[] c = { 3, 3, 10 };
+            int n = 3;
+
+            TriangleCalculator calculator = new TriangleCalculator();
+            int count = calculator.Count(a, b, c, n);
+
+            Assert.That(count, Is.EqualTo(0));
+        }
+        [Test]
+        public void Test4()
+        {
+            int[] a = { 3, 5, 6 };//равносторонний
+            int[] b = { 3, 5, 5 };//рвб
+            int[] c = { 3, 2, 4 };//разносторонний
+            int n = 3;
+
+            TriangleCalculator calculator = new TriangleCalculator();
+            int count = calculator.Count(a, b, c, n);
+
+            Assert.That(count, Is.EqualTo(3));
+        }
+        [Test]
+        public void Test5()
+        {
+            int[] a = { 3, 0, -3, 5 };
+            int[] b = { 4, 4, 4, 5 };
+            int[] c = { 5, 5, 5, 5 };
+            int n = 4;
+
+            TriangleCalculator calculator = new TriangleCalculator();
+            int count = calculator.Count(a, b, c, n);
+
+            Assert.That(count, Is.EqualTo(2));
+        }
+    }
+}
---
+++ cs2/Program.cs
@@ -0,0 +1,55 @@
+﻿//Задано n троек целых чисел (ai,bi,ci).Определить, сколько троек может быть использовано для построения треугольника со сторонами a_i, b_i, c_i.
+
+using System;
+
+namespace Triangle
+{
+    public class TriangleCalculator
+    {
+        public int Count(int[] a, int[] b, int[] c, int n)
+        {
+            int count = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (a[i] + b[i] > c[i] && a[i] + c[i] > b[i] && b[i] + c[i] > a[i])
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //взаимодействие с юзеоом
+            Console.WriteLine("Введите переменную n (количество троек чисел):");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] a = new int[n];
+            int[] b = new int[n];
+            int[] c = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Введите три числа для тройки {i + 1} через Enter:");
+                a[i] = Convert.ToInt32(Console.ReadLine());
+                b[i] = Convert.ToInt32(Console.ReadLine());
+                c[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+
+            // логика тут
+            TriangleCalculator calculator = new TriangleCalculator();
+
+            int count = calculator.Count(a, b, c, n);
+
+            string outMessage = $"Количество троек, из которых можно сделать треугольник: {count}";
+
+            //вывод
+            Console.WriteLine(outMessage);
+        }
+    }
+}
